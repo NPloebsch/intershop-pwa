7c52bf68abe68825514cc2c2aca195ab264034ad
Stefan Hauke <s.hauke@intershop.de>
2020-10-12 10:34:51 +0200

    set correct cookieConsent cookie expiration time of one year + CookiesService documentation

--- src/app/core/services/cookies/cookies.service.ts
+++ src/app/core/services/cookies/cookies.service.ts
@@ -7,6 +7,11 @@ import { COOKIE_CONSENT_OPTIONS } from 'ish-core/configurations/injection-keys';
 import { COOKIE_CONSENT_VERSION } from 'ish-core/configurations/state-keys';
 import { CookieConsentOptions, CookieConsentSettings } from 'ish-core/models/cookies/cookies.model';
 
+/**
+ * The Cookies Service handles any interaction of the PWA with cookies.
+ * It is a wrapper to the basic cookie handling with the 'ngx-utils-cookies-port'.
+ * in addition it provides methods for the cookie consent handling.
+ */
 @Injectable({ providedIn: 'root' })
 export class CookiesService {
   constructor(
@@ -28,18 +33,33 @@ export class CookiesService {
     this.cookiesService.put(key, value, options);
   }
 
+  /**
+   * Saves the given cookie consent options settings together with the current cookie consent version
+   * to a users cookie named 'cookieConsent' and reloads the PWA application with the new settings.
+   * @param options The selected cookie consent options that should be enabled.
+   */
   setCookiesConsentFor(options: string[]) {
     const cookieConsentVersion = this.transferState.get<number>(COOKIE_CONSENT_VERSION, 1);
     this.deleteAllCookies();
-    this.put('cookieConsent', JSON.stringify({ enabledOptions: options, version: cookieConsentVersion }));
+    this.put('cookieConsent', JSON.stringify({ enabledOptions: options, version: cookieConsentVersion }), {
+      expires: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+    });
     window.location.reload();
   }
 
+  /**
+   * Saves all configured cookie consent options as the users cookie consent settings.
+   */
   setCookiesConsentForAll() {
     this.setCookiesConsentFor(this.cookieConsentOptions?.options.map(x => x.id));
   }
 
-  cookieConsentFor(option: string) {
+  /**
+   * Determines the current users cookie consent setting for the requested cookie consent option.
+   * @param option The cookie consent option of interest.
+   * @returns      'true' if the user has given the cookie consent for the requested option, 'false' otherwise.
+   */
+  cookieConsentFor(option: string): boolean {
     if (isPlatformBrowser(this.platformId)) {
       const cookieConsentSettings = JSON.parse(
         this.cookiesService.get('cookieConsent') || 'null'
@@ -50,6 +70,9 @@ export class CookiesService {
     }
   }
 
+  /**
+   * Deletes all cookies except for the ones configured as 'allowedCookies' in the environments cookie consent options.
+   */
   private deleteAllCookies() {
     const allCookies = this.cookiesService.getAll();
     for (const cookie in allCookies) {
